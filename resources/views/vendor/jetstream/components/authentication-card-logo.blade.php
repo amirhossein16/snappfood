9025ed72d363a57864faf3e845fc308b430d986e
amirhossein16 <amirhosseinir1688@gmail.com>
2022-07-29 12:03:27 +0430

    Fix Login Bugs

---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,14 @@
+<div class="hidden xl:flex flex-col min-h-screen">
+    <a href="" class="-intro-x flex items-center pt-5">
+        <img alt="Icewall Tailwind HTML Admin Template" class="w-6" src="dist/images/logo.svg">
+        <span class="text-white text-lg mr-3 adjust "> ایس<span class="font-medium">وال</span> </span>
+    </a>
+    <div class="my-auto">
+        <img alt="Icewall Tailwind HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="dist/images/illustration.svg">
+        <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">
+            تنها چند کلیک
+            <br> مانده تا ساخت اکانتتان
+        </div>
+        <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-gray-500">تمامی اکانت های خود را در یک مکان مدیریت کنید</div>
+    </div>
+</div>
---
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -0,0 +1,9 @@
+<div class="block xl:grid grid-cols-2 gap-4">
+    <div>
+        {{ $LoginInfo }}
+    </div>
+
+    <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
+        {{ $slot }}
+    </div>
+</div>
