e9f9eed672950faf464967030157b31587eeb018
amirhossein16 <amirhosseinir1688@gmail.com>
2022-07-29 15:23:24 +0430

    Added Seeder Part 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(5)->create();
+        \App\Models\restaurantCategories::factory(10)->create();
+        \App\Models\foodCategories::factory(10)->create();
         $this->call(RolePermissionSedder::class);
         $this->call(SuperAdminSeeder::class);
         $this->call(RestaurantCategory::class);
